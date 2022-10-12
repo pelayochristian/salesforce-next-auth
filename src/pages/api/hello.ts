6215862be90bd30a0e0473947979cdeb931338a9
Christian Pelayo <pelayochristian.dev@gmail.com>
2022-10-12 23:47:48 +0800

    test hello session

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,21 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { unstable_getServerSession } from 'next-auth'
 import { authOptions } from './auth/[...nextauth]'
+import { unstable_getServerSession } from "next-auth/next"
+import { getSession } from 'next-auth/react'
+
 
 type Data = {
-  name: string
+	name: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
 ) {
-  const sesssion = unstable_getServerSession(req, res, authOptions)
-  if (!sesssion) {
-    res.status(401).json({
-      message: 'Unauthorized'
-    })
-  }
-  res.status(200).json({ name: 'John Doe' })
+	const session = await getSession({ req })
+	console.log('session', session);
+	if (!session) {
+		return res.status(401).json({
+			message: 'Unauthorized'
+		});
+	}
+	res.status(200).json({ name: 'John Doe' })
 }
